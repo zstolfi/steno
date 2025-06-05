767a1d84454e0d725937820aa3b27c703b5c1842
TheZacher5645 <zander5645@yahoo.com>
2025-06-05 18:04:21 -0500

    Recompile + format

--- atlas/src/window.hh
+++ atlas/src/window.hh
@@ -25,35 +25,35 @@ public:
 		}
 
 		// Decide GL+GLSL versions
-	#if defined(IMGUI_IMPL_OPENGL_ES2)
+#	if defined(IMGUI_IMPL_OPENGL_ES2)
 		// GL ES 2.0 + GLSL 100 (WebGL 1.0)
 		const char* glsl_version = "#version 100";
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-	#elif defined(IMGUI_IMPL_OPENGL_ES3)
+#	elif defined(IMGUI_IMPL_OPENGL_ES3)
 		// GL ES 3.0 + GLSL 300 es (WebGL 2.0)
 		const char* glsl_version = "#version 300 es";
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-	#elif defined(__APPLE__)
+#	elif defined(__APPLE__)
 		// GL 3.2 Core + GLSL 150
 		const char* glsl_version = "#version 150";
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-	#else
+#	else
 		// GL 3.0 + GLSL 130
 		const char* glsl_version = "#version 130";
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-	#endif
+#	endif
 
 		// Create window with graphics context
 		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
@@ -156,8 +156,8 @@ private:
 				event.preventDefault(); setDragOver(false);
 				const sendFile = (file) => {
 					let reader = new FileReader();
-					reader.onload = () => transmitFile(file.name, file.size, new Uint8Array(reader.result));
 					reader.onerror = () => alert(`Upload of ${file.name} failed.`);
+					reader.onload = () => transmitFile(file.name, file.size, new Uint8Array(reader.result));
 					reader.readAsArrayBuffer(file);
 				};
 				if (event.dataTransfer.items) {
