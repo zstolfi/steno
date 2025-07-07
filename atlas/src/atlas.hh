febf6674015b716af580dc0ae3299c85c6dca316
zstolfi <zander5645@gmail.com>
2025-07-07 18:58:28 -0500

    Rewrite Atlas constructor
    Per-pixel wasn't the way to go. (Go figure)
    Now it looks through each entry, and plots it if it's a one-stroker.

--- atlas/src/atlas.hh
+++ atlas/src/atlas.hh
@@ -9,6 +9,8 @@
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
+namespace math {
+
 // Line -> Square
 auto hilbert(unsigned val) -> std::array<unsigned, 2> {
 	std::array<unsigned, 2> pos {0, 0};
@@ -44,38 +46,11 @@ auto hilbert_inv(std::array<unsigned, 2> pos) -> unsigned {
 		}
 		val += s*s * quadrant;
 	}
-	
+
 	return val;
 }
 
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-steno::Stroke fromCustomBitOrdering(std::bitset<22> b) {
-	steno::Stroke result {};
-	result.keys.S_  = b[21];
-	result.keys.T_  = b[20];
-	result.keys.K_  = b[19];
-	result.keys.P_  = b[18];
-	result.keys.W_  = b[17];
-	result.keys.H_  = b[16];
-	result.keys.R_  = b[15];
-	result.keys.A   = b[14];
-	result.keys.O   = b[13];
-	result.keys.E   = b[12];
-	result.keys.U   = b[11];
-	result.keys.x   = b[10];
-	result.keys._F  = b[ 9];
-	result.keys._R  = b[ 8];
-	result.keys._P  = b[ 7];
-	result.keys._B  = b[ 6];
-	result.keys._L  = b[ 5];
-	result.keys._G  = b[ 4];
-	result.keys._T  = b[ 3];
-	result.keys._S  = b[ 2];
-	result.keys._D  = b[ 1];
-	result.keys._Z  = b[ 0];
-	return result;
-}
+} // namespace math
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
@@ -84,57 +59,81 @@ struct Atlas {
 	Atlas() = default;
 
 	static constexpr unsigned N = 2048;
-	Atlas(steno::Dictionary dict) : image(N*N*4, {}) {
-		for (std::size_t i=0; i<=N*N; i++) {
-			auto x = i%N;
-			auto y = i/N;
-			y = N-1 - y;
+	Atlas(steno::Dictionary dict): image(4*N*N, {}) {
+		std::for_each(dict.begin(), dict.end(), [this] (auto entry) {
+			auto const& [strokes, text] = entry;
+			if (strokes.list.size() != 1) return;
+			// Ignore number bar strokes ... for now.
+			if (strokes.list[0].keys.Num) return;
+			auto bitValue = customBitOrdering(strokes.list[0]);
+			auto [posX, posY] = math::hilbert(bitValue);
 
-			auto h = hilbert_inv({x, y});
-			auto iter = dict.find(fromCustomBitOrdering(h));
-
-			std::array<uint8_t, 3> rgb {};
-			static const std::array<std::array<uint8_t, 3>, 26> hues {{
-				/*a*/ {229,  25,  25},
-				/*b*/ {229,  72,  25},
-				/*c*/ {229, 119,  25},
-				/*d*/ {229, 166,  25},
-				/*e*/ {229, 213,  25},
-				/*f*/ {198, 229,  25},
-				/*g*/ {151, 229,  25},
-				/*h*/ {103, 229,  25},
-				/*i*/ { 56, 229,  25},
-				/*j*/ { 25, 229,  41},
-				/*k*/ { 25, 229,  88},
-				/*l*/ { 25, 229, 135},
-				/*m*/ { 25, 229, 182},
-				/*n*/ { 25, 229, 229},
-				/*o*/ { 25, 182, 229},
-				/*p*/ { 25, 135, 229},
-				/*q*/ { 25,  88, 229},
-				/*r*/ { 25,  41, 229},
-				/*s*/ { 56,  25, 229},
-				/*t*/ {103,  25, 229},
-				/*u*/ {151,  25, 229},
-				/*v*/ {198,  25, 229},
-				/*w*/ {229,  25, 213},
-				/*x*/ {229,  25, 166},
-				/*y*/ {229,  25, 119},
-				/*z*/ {229,  25, 72},
-			}};
-
-			if (iter == dict.end()) rgb = {0, 0, 0};
-			else {
-				char c = iter->second[0];
-				/**/ if ('a' <= c&&c <= 'z') rgb = hues[c-'a'];
-				else if ('A' <= c&&c <= 'Z') rgb = hues[c-'A'];
-				else rgb = {255, 255, 255};
+			std::array<uint8_t, 3> rgb {255, 255, 255};
+			if (char const c = text[0]) {
+				if ('a' <= c&&c <= 'z') rgb = hues[c - 'a'];
+				if ('A' <= c&&c <= 'Z') rgb = hues[c - 'A'];
 			}
 
+			auto i = N * (N-1 - posY) + posX;
 			image[4*i+0] = rgb[0];
 			image[4*i+1] = rgb[1];
 			image[4*i+2] = rgb[2];
 			image[4*i+3] = 255;
-		}
+		});
+	}
+
+private:
+	static constexpr std::array<std::array<uint8_t, 3>, 26> hues {{
+		/*a*/ {229,  25,  25},
+		/*b*/ {229,  72,  25},
+		/*c*/ {229, 119,  25},
+		/*d*/ {229, 166,  25},
+		/*e*/ {229, 213,  25},
+		/*f*/ {198, 229,  25},
+		/*g*/ {151, 229,  25},
+		/*h*/ {103, 229,  25},
+		/*i*/ { 56, 229,  25},
+		/*j*/ { 25, 229,  41},
+		/*k*/ { 25, 229,  88},
+		/*l*/ { 25, 229, 135},
+		/*m*/ { 25, 229, 182},
+		/*n*/ { 25, 229, 229},
+		/*o*/ { 25, 182, 229},
+		/*p*/ { 25, 135, 229},
+		/*q*/ { 25,  88, 229},
+		/*r*/ { 25,  41, 229},
+		/*s*/ { 56,  25, 229},
+		/*t*/ {103,  25, 229},
+		/*u*/ {151,  25, 229},
+		/*v*/ {198,  25, 229},
+		/*w*/ {229,  25, 213},
+		/*x*/ {229,  25, 166},
+		/*y*/ {229,  25, 119},
+		/*z*/ {229,  25,  72},
+	}};
+
+	static uint32_t customBitOrdering(steno::Stroke x) {
+		return x.keys.S_ << 21
+		|      x.keys.T_ << 20
+		|      x.keys.K_ << 19
+		|      x.keys.P_ << 18
+		|      x.keys.W_ << 17
+		|      x.keys.H_ << 16
+		|      x.keys.R_ << 15
+		|      x.keys.A  << 14
+		|      x.keys.O  << 13
+		|      x.keys.E  << 12
+		|      x.keys.U  << 11
+		|      x.keys.x  << 10
+		|      x.keys._F <<  9
+		|      x.keys._R <<  8
+		|      x.keys._P <<  7
+		|      x.keys._B <<  6
+		|      x.keys._L <<  5
+		|      x.keys._G <<  4
+		|      x.keys._T <<  3
+		|      x.keys._S <<  2
+		|      x.keys._D <<  1
+		|      x.keys._Z <<  0;
 	}
 };
